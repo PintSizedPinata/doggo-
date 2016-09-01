97c20f078d982bce961471d981e1dbbfe51d85b8
Pina <Pina@DESKTOP-MRPBHLG>
2016-09-01 14:53:03 -0500

    Add project files.

---
+++ ConsoleApplication3/Sourace.cpp
@@ -0,0 +1,34 @@
+// DOGGO PLEASE
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome to the Doggo Questionairre 2.0" << endl;
+	cout << "How many doggos do you happen to posssess?" << endl << endl;
+	int input;
+	cin >> input;
+	if (input > 1 && input < 69) {
+		cout << "holy mackerayna thats more doggos then even I have!!!!" << endl << endl;
+		cin.get();
+		cout << "Thanks for playing";
+	}
+	else if (input == 69) {
+		cout << "holly shit" << endl << endl;
+		cin.get();
+		cout << "Thanks for playing";
+	}
+	else if (input == 0) {
+		cout << "how sad ima cry \;\(" << endl << endl;
+		cin.get();
+		cout << "go fuck urself";
+
+	}
+	else {
+		cout << "bad answer" << endl << endl;
+		cin.get();
+	}
+	cin.get();
+}
---
+++ ConsoleApplication3/Source.cpp
@@ -0,0 +1,37 @@
+//why wont this work
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << " Hello World!" << endl;
+
+	cout << "How many doggos you got good chum????" << endl < endl < endl;
+	string buffer;
+	char input;
+	cin >> input;
+	if (input == '1') {
+		cout << "wow das more pups then i got" << endl << endl;
+			cin.get();
+	}
+	string buffer;
+	char input;
+	cin >> input;
+	if (input == '69') {
+		cout << "whoa thats the magic number bucker" << endl << endl;
+		cin.get();
+	}
+	string buffer;
+	char input;
+	cin >> input;
+	if (input == 'zero') {
+		cout << "thats horribly sad sorry I asked";
+			cin.get();
+	}
+	else {
+		cout << "answer the damn question you goon" << endl;
+		cin.get();
+	}
+	cin.get();
+}
